da8683ee75df1d4d02eb883aff8d739140b60982
manisha <manisha.sharma@ucodesoft.com>
2023-01-23 18:36:52 +0530

    design created navigation done

--- app/src/main/java/com/example/instagramdemo/MainActivity.kt
+++ app/src/main/java/com/example/instagramdemo/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.instagramdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.instagramdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  viewBinding:ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+
+
+        viewBinding.bottomNavView.setupWithNavController(navController = navController)
+
     }
+
 }
